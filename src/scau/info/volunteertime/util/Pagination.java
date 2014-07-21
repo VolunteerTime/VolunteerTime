01e8c6fc56b003a40f49532fa570e1be330f4622
chaoKing <786773088@qq.com>
2014-07-21 21:44:22 +0800

    完善分页类

--- src/scau/info/volunteertime/util/Pagination.java
+++ src/scau/info/volunteertime/util/Pagination.java
@@ -5,10 +5,57 @@
  */
 package scau.info.volunteertime.util;
 
+import java.util.List;
+
 /**
  * @author 蔡超敏
- *
+ * 
  */
-public class Pagination {
+public class Pagination<T> {
+
+	private List<T> records; // 当前分页的记录集合
+
+	private int currentPageNumber; // 当前页的页码
+
+	private int amountOfRecorders; // 记录总数
+
+	private int pageSize; // 每页的记录数
+
+	public List<T> getRecords() {
+		return records;
+	}
+
+	public void setRecords(List<T> records) {
+		this.records = records;
+	}
+
+	public int getCurrentPageNumber() {
+		return currentPageNumber;
+	}
+
+	public void setCurrentPageNumber(int currentPageNumber) {
+		this.currentPageNumber = currentPageNumber;
+	}
+
+	public int getAmountOfRecorders() {
+		return amountOfRecorders;
+	}
+
+	public void setAmountOfRecorders(int amountOfRecorders) {
+		this.amountOfRecorders = amountOfRecorders;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getSumPage() {
+		double sumPage = ((double) amountOfRecorders) / pageSize;
+		return (int) Math.ceil(sumPage);
+	}
 
 }
