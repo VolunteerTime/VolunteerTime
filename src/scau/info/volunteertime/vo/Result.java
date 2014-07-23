1156055d71b470f31c51ee6588a3f77c66b79c33
chaoKing <786773088@qq.com>
2014-07-23 20:51:23 +0800

    完善Result

--- src/scau/info/volunteertime/vo/Result.java
+++ src/scau/info/volunteertime/vo/Result.java
@@ -5,10 +5,105 @@
  */
 package scau.info.volunteertime.vo;
 
+import java.sql.Date;
+
 /**
+ * 成果展示的VO
+ * 
  * @author 蔡超敏
- *
+ * 
  */
 public class Result {
+
+	private String title;// 题目
+	private String content;// 内容
+	private String image;// 图片
+	private String editor;// 作者
+	private Date publishTime;// 发布时间
+
+	public Result(String title, String content, String image, String editor,
+			Date publishTime) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.image = image;
+		this.editor = editor;
+		this.publishTime = publishTime;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *            the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @param content
+	 *            the content to set
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image
+	 *            the image to set
+	 */
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * @return the editor
+	 */
+	public String getEditor() {
+		return editor;
+	}
+
+	/**
+	 * @param editor
+	 *            the editor to set
+	 */
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+
+	/**
+	 * @return the publishTime
+	 */
+	public Date getPublishTime() {
+		return publishTime;
+	}
+
+	/**
+	 * @param publishTime
+	 *            the publishTime to set
+	 */
+	public void setPublishTime(Date publishTime) {
+		this.publishTime = publishTime;
+	}
 
 }
