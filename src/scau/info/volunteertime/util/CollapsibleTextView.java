139570b4c884bd2354ffbb3f98ea7fd34232f8ed
chaoKing <786773088@qq.com>
2014-07-23 21:00:37 +0800

    创建一个可折叠的TextView控件

---
+++ src/scau/info/volunteertime/util/CollapsibleTextView.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 华南农业大学信息学院蔡超敏2014版权所有
+ * 
+ * 文件创建时间：2014-7-23
+ */
+package scau.info.volunteertime.util;
+
+/**
+ * 
+ * 可折叠TextView控件
+ * 
+ * @author 蔡超敏
+ *
+ */
+public class CollapsibleTextView {
+
+}
