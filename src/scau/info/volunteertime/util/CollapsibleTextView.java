33fc7d5882ab0d1c3418bd8c2f1221fe42dcd756
chaoKing <786773088@qq.com>
2014-07-23 21:05:59 +0800

    完善可折叠控件

--- src/scau/info/volunteertime/util/CollapsibleTextView.java
+++ src/scau/info/volunteertime/util/CollapsibleTextView.java
@@ -5,13 +5,101 @@
  */
 package scau.info.volunteertime.util;
 
+import scau.info.volunteertime.R;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TextView.BufferType;
+
 /**
  * 
  * 可折叠TextView控件
  * 
  * @author 蔡超敏
- *
+ * 
  */
-public class CollapsibleTextView {
+public class CollapsibleTextView extends LinearLayout implements
+		OnClickListener {
 
+	/** default text show max lines */
+	private static final int DEFAULT_MAX_LINE_COUNT = 2;
+
+	private static final int COLLAPSIBLE_STATE_NONE = 0;
+	private static final int COLLAPSIBLE_STATE_SHRINKUP = 1;
+	private static final int COLLAPSIBLE_STATE_SPREAD = 2;
+
+	private TextView desc;
+	private TextView descOp;
+
+	private String shrinkup;
+	private String spread;
+	private int mState;
+	private boolean flag;
+
+	public CollapsibleTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		shrinkup = context.getString(R.string.desc_shrinkup);
+		spread = context.getString(R.string.desc_spread);
+		View view = inflate(context, R.layout.collapsible_textview, this);
+		desc = (TextView) view.findViewById(R.id.desc_tv);
+		descOp = (TextView) view.findViewById(R.id.desc_op_tv);
+		descOp.setOnClickListener(this);
+	}
+
+	public CollapsibleTextView(Context context) {
+		this(context, null);
+	}
+
+	/**
+	 * 设置显示内容为charSequence，若charSequence过长则会折叠后面的内容，并提供显示更多的按钮
+	 * 
+	 * @param charSequence
+	 * @param bufferType
+	 */
+	public final void setDesc(CharSequence charSequence, BufferType bufferType) {
+		desc.setText(charSequence, bufferType);
+		mState = COLLAPSIBLE_STATE_SPREAD;
+		requestLayout();
+	}
+
+	@Override
+	public void onClick(View v) {
+		flag = false;
+		requestLayout();
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		super.onLayout(changed, l, t, r, b);
+		if (!flag) {
+			flag = true;
+			if (desc.getLineCount() <= DEFAULT_MAX_LINE_COUNT) {
+				mState = COLLAPSIBLE_STATE_NONE;
+				descOp.setVisibility(View.GONE);
+				desc.setMaxLines(DEFAULT_MAX_LINE_COUNT + 1);
+			} else {
+				post(new InnerRunnable());
+			}
+		}
+	}
+
+	class InnerRunnable implements Runnable {
+		@Override
+		public void run() {
+			if (mState == COLLAPSIBLE_STATE_SPREAD) {
+				desc.setMaxLines(DEFAULT_MAX_LINE_COUNT);
+				descOp.setVisibility(View.VISIBLE);
+				descOp.setText(spread);
+				mState = COLLAPSIBLE_STATE_SHRINKUP;
+			} else if (mState == COLLAPSIBLE_STATE_SHRINKUP) {
+				desc.setMaxLines(Integer.MAX_VALUE);
+				descOp.setVisibility(View.VISIBLE);
+				descOp.setText(shrinkup);
+				mState = COLLAPSIBLE_STATE_SPREAD;
+			}
+		}
+	}
 }
