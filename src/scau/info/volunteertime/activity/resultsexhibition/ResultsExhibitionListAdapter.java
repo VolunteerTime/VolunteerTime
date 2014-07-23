1680f7bad01a0c71b4ab34ce4d820ea3f1944128
chaoKing <786773088@qq.com>
2014-07-23 22:30:44 +0800

    创建一个ResultsExhibition中ViewList的Adapter

---
+++ src/scau/info/volunteertime/activity/resultsexhibition/ResultsExhibitionListAdapter.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 华南农业大学信息学院蔡超敏2014版权所有
+ * 
+ * 文件创建时间：2014-7-23
+ */
+package scau.info.volunteertime.activity.resultsexhibition;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * @author 蔡超敏
+ *
+ */
+public class ResultsExhibitionListAdapter extends BaseAdapter {
+
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getCount()
+	 */
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getItem(int)
+	 */
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getItemId(int)
+	 */
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getView(int, android.view.View, android.view.ViewGroup)
+	 */
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
