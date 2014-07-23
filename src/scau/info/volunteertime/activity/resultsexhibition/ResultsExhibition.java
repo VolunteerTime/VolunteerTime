e9b347d174536f4f8ca32044fcc24c844aa3603a
chaoKing <786773088@qq.com>
2014-07-23 23:25:01 +0800

    完善ResultsExhibition

--- src/scau/info/volunteertime/activity/resultsexhibition/ResultsExhibition.java
+++ src/scau/info/volunteertime/activity/resultsexhibition/ResultsExhibition.java
@@ -5,26 +5,91 @@
  */
 package scau.info.volunteertime.activity.resultsexhibition;
 
+import cn.trinea.android.common.view.DropDownListView;
 import scau.info.volunteertime.R;
+import scau.info.volunteertime.business.ResultBO;
+import scau.info.volunteertime.util.Pagination;
+import scau.info.volunteertime.vo.Result;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 /**
+ * 成果展示的activity
+ * 
  * @author 蔡超敏
  * 
  */
 public class ResultsExhibition extends ActionBarActivity {
 
+	private Pagination<Result> resultsPagination;// 装载当前内容
+	private Pagination<Result> nextResultsPagination;// 作为装载下一页内容的中介
+
+	private DropDownListView resultsListView;
+
+	private ResultsExhibitionListAdapter resultsExhibitionListAdapter;
+
+	private ResultBO resultBO;
+
+	private boolean hasMore;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_results_exhibition);
+
+		Log.d("ResultsExhibition", "create");
+
+		hasMore = true;// 有更多才显示，没有则不显示
+
+		resultBO = new ResultBO();// 取得resultBO
+		resultsPagination = new Pagination<Result>();
+
+		resultsExhibitionListAdapter = new ResultsExhibitionListAdapter(this,
+				resultsPagination);// 成果数据传给Adapter
+
+		resultsListView.setAdapter(resultsExhibitionListAdapter);
+
+		resultsListView
+				.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+					@Override
+					public boolean onItemLongClick(AdapterView<?> parent,
+							View view, int position, long id) {
+						Log.d("ResultsExhibition-onCreate-setOnItemLongClickListener-onItemLongClick",
+								"初步测试");
+						return false;
+					}
+
+				});
+		resultsListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Log.d("ResultsExhibition-onCreate-setOnItemClickListener-onItemClick",
+						"初步测试");
+			}
+		});
+
+		resultsListView.setOnBottomListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Log.d("ResultsExhibition-onCreate-setOnBottomListener-onClick",
+						"初步测试");
+			}
+		});
+		
+		resultsListView.onBottom();
+
 	}
 
 	@Override
@@ -45,23 +110,6 @@ public class ResultsExhibition extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(
-					R.layout.fragment_results_exhibition, container, false);
-			return rootView;
-		}
 	}
 
 }
