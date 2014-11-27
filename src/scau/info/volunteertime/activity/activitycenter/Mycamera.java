ec5da4b8f9f67fbb9530a7a0b2bb0cdd310042ef
xin <497668869@qq.com>
2014-11-27 14:27:41 +0800

    3

---
+++ src/scau/info/volunteertime/activity/activitycenter/Mycamera.java
@@ -0,0 +1,49 @@
+package scau.info.volunteertime.activity.activitycenter;
+
+
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Transformation;
+
+//报名的旋转特效
+public class Mycamera extends Animation {
+	int centerX, centerY;
+	boolean isZoomin;
+	Camera camera = new Camera();
+	public Mycamera(boolean isZoomin) {
+		this.isZoomin = isZoomin;
+	}
+	@Override
+	public void initialize(int width, int height, int parentWidth,
+			int parentHeight) {
+		super.initialize(width, height, parentWidth, parentHeight);
+		
+		centerX = width / 2;
+		centerY = height / 2;
+		setDuration(400); 
+		setFillAfter(true); 
+		setInterpolator(new DecelerateInterpolator()); 
+	}
+
+	@Override
+	protected void applyTransformation(float interpolatedTime, Transformation t) {
+		final Matrix matrix = t.getMatrix();
+		int direction = -1;
+		camera.save(); 
+		if (!isZoomin) {
+			interpolatedTime = 1 - interpolatedTime;
+			direction = 1;
+		}
+		
+		camera.translate(0, 0, 200 + 200.0f * (interpolatedTime-1)); //x,y,z���ƫ����
+		camera.rotateY(90 * interpolatedTime * direction); //��������Y����ת
+		camera.getMatrix(matrix);//�����ǵ�����ͷ���ڱ任������
+		matrix.preTranslate(-centerX, -centerY); 
+		matrix.postTranslate(centerX, centerY);
+		camera.restore(); 
+	}
+
+}
